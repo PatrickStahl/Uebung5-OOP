691697711cd155244a5e6e30e01f454f9a11b277
Patrick <patrick.stahl@uni-jena.de>
2022-06-06 17:27:30 +0200

    first commit

---
+++ AutoTest.java
@@ -0,0 +1,157 @@
+class Auto
+{
+    public  String Kennzeichen = "J-AA 01";
+    public  int Kilometerstand = 0;
+    public  int Sitzplätze = 5;
+    public  String Antenne = "eingefahren"; //0 = eingefahren 1 = ausgefahren
+    
+
+    public Auto()
+    {
+        
+    }
+
+    public Auto(String Kennzeichen)
+    {
+        this.Kennzeichen = Kennzeichen;
+    }
+
+    public String getKennzeichen()
+    {
+        System.out.println(Kennzeichen);
+        return Kennzeichen;
+    }
+
+    public int getKilometerstand()
+    {
+        return Kilometerstand;
+    }
+
+    public int getSitzplaetze()
+    {
+        return Sitzplätze;
+    }
+
+    public void fahre(int Kilometer)
+    {
+        System.out.println(Kilometer +" gefahren!");
+        Kilometerstand = Kilometerstand + Kilometer;
+    }
+
+    public void fahreAntenneAus()
+    {
+        System.out.println("Antenne ausgefahren!");
+        Antenne = "ausgefahren";
+    }
+
+    public void fahreAntenneEin()
+    {
+        System.out.println("Antenne eingefahren!");
+        Antenne = "eingefahren";
+    }
+
+    public void bereiteWaschenVor()
+    {
+        System.out.println("Waschen wird vorbereitet...");
+        System.out.println("Antenne zum Waschen eingefahren!");
+        Antenne = "eingefahren";
+    }
+
+    public void wasche()
+    {
+        System.out.println("Auto wird gewaschen!");
+    }
+
+    public String toString()
+    {
+        return Kennzeichen + ": " + Kilometerstand + " Kilometer gefahren, " + Sitzplätze + " Sitzplätze, " + "Antenne " + Antenne;
+    }
+}
+
+class PickUp extends Auto
+{
+    public int f;
+    public int ladungPickUp;
+    public PickUp(int Fassungsvermoegen)
+    {
+        this.f=Fassungsvermoegen;
+        this.Sitzplätze=2;
+    }
+
+    public PickUp(String Kennzeichen, int Fassungsvermoegen)
+    {
+        this.f=Fassungsvermoegen;
+        this.Kennzeichen = Kennzeichen;
+        this.Sitzplätze = 2;
+    }
+
+    public int getLadung()
+    {
+        return ladungPickUp;
+    }
+
+    public boolean beladen(int ladung)
+    {
+        if(f>=ladung)
+        {
+            System.out.println("Pick-Up wurde erfolgreich beladen!");
+            this.ladungPickUp = ladung;
+            return true;
+        }
+        else
+        {
+            System.out.println("Fassungsvermögen reicht nicht aus!");
+            return false;
+        }
+    }
+
+    public void entladen(int ladung)
+    {
+        System.out.println("Ladung wurde um " + ladung + " verringert");
+        if(ladungPickUp-ladung<0)
+        {
+            ladungPickUp = 0;
+        }
+        else
+        {
+            ladungPickUp = ladungPickUp-ladung;
+        }
+    }
+
+    public void entladen()
+    {
+        entladen(getLadung());
+    }
+
+    public void bereiteWaschenVor()
+    {
+        System.out.println("Waschen wird vorbereitet...");
+        System.out.println("Antenne zum Waschen eingefahren!");
+        System.out.println("PickUp zum Waschen entladen!");
+        Antenne = "eingefahren";
+        entladen();
+    }
+
+    public String toString()
+    {
+        return Kennzeichen + ": " + Kilometerstand + " Kilometer gefahren, " + Sitzplätze + " Sitzplätze, " + "Antenne " + Antenne + ", " + f +" Liter Fassungsvermögen" + ", " + ladungPickUp + " Liter Ladung";
+    }
+}
+
+
+public class AutoTest
+{
+    public static void main(String[] args)
+    {
+            //die Methoden funktionieren alle, es fehlt nur noch das Array in der main Methode
+            PickUp pickup1 = new PickUp(50);
+            pickup1.beladen(39);
+            System.out.println(pickup1.toString());
+            System.out.println("");
+            pickup1.entladen(3);
+            System.out.println(pickup1.toString());
+            System.out.println("");
+            pickup1.bereiteWaschenVor();
+            System.out.println(pickup1.toString());
+    }
+}
